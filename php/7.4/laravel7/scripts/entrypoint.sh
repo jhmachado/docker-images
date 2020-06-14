a93dfa70e84c1713d780c8730d2fcd65e14be8c0
Jhonatan Machado <jh.machado12@gmail.com>
2020-06-14 16:31:51 -0300

    Improving the php:7.4-laravel entrypoint image to skip services according to the environmental variables

--- php/7.4/laravel7/scripts/entrypoint.sh
+++ php/7.4/laravel7/scripts/entrypoint.sh
@@ -4,15 +4,20 @@ set -e
 
 composer install
 
-if [ !$autobuild ]; then
-    wait-for-it.sh mysql:3306 -t 600
-    echo "MySQL connection established"
-    php artisan migrate
+if [ "${SHOULD_SKIP_MIGRATIONS}" ]; then
+  echo "Skipping database migration"
+else
+  wait-for-it.sh mysql:3306 -t 600
+  echo "MySQL connection established"
+  php artisan migrate
 fi
 
 chown -R www-data:www-data /app/storage
 chown -R www-data:www-data /app/bootstrap/cache
 
-service inetutils-syslogd start
-
-apache2-foreground
+if [ "${SHOULD_SKIP_SERVICES}" ]; then
+  echo "Skipping services start"
+else
+  service inetutils-syslogd start
+  apache2-foreground
+fi
