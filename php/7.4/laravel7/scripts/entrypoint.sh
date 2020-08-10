1380eeb0425f848934f2b7c27060ae3a37b9223f
Jhonatan Machado <jh.machado12@gmail.com>
2020-08-10 20:51:44 -0300

    Updating the laravel image to support queues, and laravel horizon, on different containers

--- php/7.4/laravel7/scripts/entrypoint.sh
+++ php/7.4/laravel7/scripts/entrypoint.sh
@@ -15,9 +15,26 @@ fi
 chown -R www-data:www-data /app/storage
 chown -R www-data:www-data /app/bootstrap/cache
 
+function start_service()
+{
+  case ${CONTAINER_ROLE} in
+    queue)
+      echo "Starting queue"
+      php /app/artisan queue:work --verbose --tries=3 --timeout=90 --queue=high,medium,low
+      ;;
+    horizon)
+      echo "Starting laravel horizon"
+      php /app/artisan horizon
+      ;;
+    *)
+      service inetutils-syslogd start
+      apache2-foreground
+      ;;
+  esac
+}
+
 if [ "${SHOULD_SKIP_SERVICES}" ]; then
   echo "Skipping services start"
 else
-  service inetutils-syslogd start
-  apache2-foreground
+  start_service
 fi
